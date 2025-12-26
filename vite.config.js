75a0a0560d60bfa0e20b1bbfe0d8b9ae1c108349
chetanrugithub <chetankpcmct2023@gmail.com>
2025-12-26 17:47:04 +0530

    Add CSS cache busting to Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
 })
